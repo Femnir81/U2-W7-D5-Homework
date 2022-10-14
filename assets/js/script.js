2562a9264f30debbb377afa5b103c27f07ad1b72
Femnir81 <inmydefence81@yahoo.it>
2022-10-14 18:18:03 +0200

    compito

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,42 +15,7 @@ async function displayLidia() {
         </div>
       </div>`
     }
-    // document.getElementById('container').innerHTML += `<div class="card" style="width: 18rem">
-    //     <div class="cardContainer">
-    //     <img src=${arrayDisney[0].imageUrl} class="card-img-top" alt="..." />
-    //     </div>
-    //     <div class="card-body">
-    //       <p class="card-text">
-    //         Ssssssssssss
-    //       </p>
-    //     </div>
-    //   </div>`
-    // document.getElementById('container').innerHTML += `<div class="card" style="width: 18rem">
-    // <div class="cardContainer">
-    //   <img src=${arrayDisney[1].imageUrl} class="card-img-top" alt="..." />
-    //   </div>
-    //   <div class="card-body">
-    //     <p class="card-text">
-    //       Some quick example text to build on the card title and make up the
-    //       bulk of the card's content.
-    //     </p>
-    //   </div>
-    // </div>`
-    // document.getElementById('container').innerHTML += `<div class="card" style="width: 18rem">
-    // <div class="cardContainer">
-    //   <img src=${arrayDisney[2].imageUrl} class="card-img-top" alt="..." />
-    //   </div>
-    //   <div class="card-body">
-    //     <p class="card-text">
-    //       Some quick example text to build on the card title and make up the
-    //       bulk of the card's content.
-    //     </p>
-    //   </div>
-    // </div>`
 }
-
-
-
 
 window.onload = async () => {
     await displayLidia()
