75a956a6b2bee6f9fa30c53f354767298fcaff69
Femnir81 <inmydefence81@yahoo.it>
2022-10-17 00:56:41 +0200

    searchbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,5 +18,32 @@ async function displayLidia() {
 }
 
 window.onload = async () => {
-    await displayLidia()
+  await displayLidia()
+}
+
+async function visualizeCards() {
+  document.getElementById('container').innerHTML = '' 
+  let fetchCharacters = await fetch('https://api.disneyapi.dev/character')
+  let objectCharacters = await fetchCharacters.json()
+  console.log(fetchCharacters)
+  let arrayCharacters = objectCharacters.data
+  console.log(arrayCharacters)
+  for (const character of arrayCharacters) {
+    let searchName = document.getElementById('searchBar').value
+    console.log(searchName)
+    if (character.name == searchName) {
+      document.getElementById('container').innerHTML += `<div class="card">
+      <div class="cardContainer">
+      <img src=${character.imageUrl} class="card-img-top" alt="..." />
+      </div>
+      <div class="card-body">
+        <p class="card-text">
+          ${character.name}
+        </p>
+      </div>
+    </div>`
+    } else if (searchName == "") {
+      return await displayLidia()      
+    }
   }
+}
